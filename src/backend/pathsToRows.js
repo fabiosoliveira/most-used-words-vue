b722c31c236cb19d4c032fb4ae3770137c405f3b
Fábio Oliveira <fabio_net1000@hotmail.com>
2020-09-24 08:54:32 -0300

    enviar arquivos pro repositório

---
+++ src/backend/pathsToRows.js
@@ -0,0 +1,16 @@
+import fs from "fs";
+
+export default paths => {
+  return new Promise((resolver, reject) => {
+    try {
+      const rows = paths
+        .map(path => fs.readFileSync(path).toString("utf-8"))
+        .reduce((fullText, fileText) => `${fullText}\n${fileText}`)
+        .split("\n");
+
+      resolver(rows);
+    } catch (e) {
+      reject(e);
+    }
+  });
+};
